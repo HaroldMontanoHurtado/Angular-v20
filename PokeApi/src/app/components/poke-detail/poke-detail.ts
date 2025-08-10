cd4c49dfe56513d3382f40653b298f3afed87dcf
HaroldMontanoHurtado <harold.montano@correounivalle.edu.co>
2025-08-10 18:34:29 -0500

    components created

---
+++ PokeApi/src/app/components/poke-detail/poke-detail.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-poke-detail',
+  imports: [],
+  templateUrl: './poke-detail.html',
+  styleUrl: './poke-detail.scss'
+})
+export class PokeDetail {
+
+}
---
+++ PokeApi/src/app/components/poke-table/poke-table.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-poke-table',
+  imports: [],
+  templateUrl: './poke-table.html',
+  styleUrl: './poke-table.scss'
+})
+export class PokeTable {
+
+}
